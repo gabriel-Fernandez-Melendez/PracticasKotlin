36aeed56d3c489ebb631e03547b3a2fc19d2d565
alumnoDAM <yic00732@educastur.es>
2025-01-21 19:25:47 +0100

    recordar completar la estructura de los botones

--- app/src/main/java/com/example/miaplicacion/componente/MisBotones.kt
+++ app/src/main/java/com/example/miaplicacion/componente/MisBotones.kt
@@ -1,20 +1,36 @@
 package com.example.miaplicacion.componente
 
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.miaplicacion.ui.theme.MiAplicacionTheme
 
 @Composable
 fun botonSimple(modifier: Modifier=Modifier){
     Button(onClick = {}, //se que vacio de momento
-    modifier = modifier
+    modifier = modifier,
+        colors = ButtonDefaults.buttonColors(contentColor = Color.Blue, containerColor =    Color.Red)
     ) {
         Text(
             text = "primerBoton",
-            fontSize = 10.sp //era con sp no con dp 
+            fontSize = 30.sp //era con sp no con dp
         )
     }
+}
+
+@Composable
+fun botones(){
+
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview2() {
+    botonSimple(Modifier)
 }
