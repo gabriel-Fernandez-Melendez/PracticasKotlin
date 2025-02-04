da41f9a1474a9c0d0c0a7d231f0057a920c936af
alumnoDAM <yic00732@educastur.es>
2025-02-04 19:27:22 +0100

    cambios en la funcion de switch

--- app/src/main/java/com/example/miaplicacion/vista/BarraDeProgreso.kt
+++ app/src/main/java/com/example/miaplicacion/vista/BarraDeProgreso.kt
@@ -1,7 +1,9 @@
 package com.example.miaplicacion.vista
 
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -78,9 +80,47 @@ fun BarraDeProgresoDetermianda(modifier: Modifier =Modifier){
     }
 }
 
+@Composable
+fun BarraDeProgresoDetermiandaAnimacion(modifier: Modifier =Modifier){
+    Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+        var progreso by remember { mutableStateOf(0.0f) }
+        LaunchedEffect((true)  ) {
+            for(i in 0 ..100 step 10){
+                delay(300)
+                progreso =i/100f
+            }
+        }
+        val animacion by animateFloatAsState(targetValue = progreso,
+            animationSpec = ProgressIndicatorDefaults.ProgressAnimationSpec)//OJO esta es la forma de hacer una operacion dentro de un  string
+        Text("Sincronizando Datos ${(progreso*100)} ") //muestra el avanse de la barra que se devidie por partes gracias al step
+        Spacer(modifier.size(30.dp))
+        //asi se llama al atributo de una barra de carga indeterminada
+        LinearProgressIndicator(progress = animacion ,color = Color(33,150,243), //hay que poner la variable de la animacion en el argumento de process
+            trackColor = Color(0, 255, 0, 255))
+    }
+}
+
+@Composable
+fun BarraCircularConBox(modifier: Modifier =Modifier){ //revisar  esta funcion
+    Box(modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+        var progreso by remember { mutableStateOf(0.0f) }
+        LaunchedEffect((true)) {
+            for(i in 0 ..100 step 10){
+                delay(300)
+                progreso =i/100f
+            }
+        }
+        var progresoCircular by remember { mutableStateOf(0.0f) }
+        CircularProgressIndicator(progress = progresoCircular,
+           color = Color(33,150,243), //hay que poner la variable de la animacion en el argumento de process
+            trackColor = Color(0, 255, 0, 255))
+
+    }
+}
+
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreviewradiobuttond() {
-    BarraDeProgresoDetermianda(Modifier)
+    BarraCircularConBox(Modifier)
 }
