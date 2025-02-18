d2011368a5d2bb8173d6a51d028b5cd4f8c38656
alumnoDAM <yic00732@educastur.es>
2025-02-18 17:52:02 +0100

    cambios a la practica instagram

--- app/src/main/java/com/example/miaplicacion/vista/InstagramLogin.kt
+++ app/src/main/java/com/example/miaplicacion/vista/InstagramLogin.kt
@@ -1,7 +1,10 @@
 package com.example.miaplicacion.vista
 
+import android.app.Activity
+import android.service.autofill.OnClickAction
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -12,6 +15,9 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -23,6 +29,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
@@ -33,35 +40,82 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun Login_Instagram(modifier: Modifier = Modifier) {
-    var texto_login by remember {  mutableStateOf("") }
-    var d by remember {  mutableStateOf("") }
-    var vardiable by remember {  mutableStateOf("") }
-    Column(modifier = Modifier
-        .background(Color(119, 6, 84, 255))
-        .fillMaxWidth()
-        .size(46.dp)) { Text(text = "")  }
-    Column(horizontalAlignment = Alignment.Start, modifier = modifier.padding(start = 360.dp, top = 50.dp)) {Icon(Icons.Default.Clear, contentDescription = " ")  }
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .fillMaxWidth(),
+    var texto_login by remember { mutableStateOf("") }
+    var contraseña by remember { mutableStateOf(false) }
+    var vardiable by remember { mutableStateOf("") }
+    Column(
+        modifier = Modifier
+            .background(Color(119, 6, 84, 255))
+            .fillMaxWidth()
+            .size(46.dp)
+    ) { Text(text = "") }
+    Column(
+        horizontalAlignment = Alignment.Start,
+        modifier = modifier.padding(start = 360.dp, top = 50.dp)
+    ) { Icon(Icons.Default.Clear, contentDescription = " ", Modifier.clickable { }) }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .fillMaxWidth(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
 
-    )
+        )
     {
 
 
-    Image(modifier = Modifier.size(200.dp), contentDescription = "intagram", painter = painterResource(id = com.example.miaplicacion.R.drawable.instagram))
-        TextField(value =texto_login, onValueChange = {texto_login = it}, label = { Text("Email")}, shape = RoundedCornerShape(8.dp), modifier = Modifier.fillMaxWidth().padding(start = 8.dp, end = 8.dp))
+        Image(
+            modifier = Modifier.size(200.dp),
+            contentDescription = "intagram",
+            painter = painterResource(id = com.example.miaplicacion.R.drawable.instagram)
+        )
+        TextField(value = texto_login,
+            onValueChange = { texto_login = it },
+            label = { Text("Email") },
+            shape = RoundedCornerShape(8.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 8.dp, end = 8.dp)
+        )
         Spacer(modifier.size(20.dp))
-        TextField(value =texto_login, onValueChange = {texto_login = it}, label = { Text("Password")}, shape = RoundedCornerShape(8.dp), modifier = Modifier.fillMaxWidth().padding(start = 8.dp, end = 8.dp))
-        Column(horizontalAlignment = Alignment.Start, modifier = modifier.padding(start = 270.dp, top = 10.dp, end = 8.dp)) { Text(text = "Forgot password?", color = Color.Blue, fontWeight = FontWeight.Bold)  }
+        TextField(value = texto_login,
+            onValueChange = { texto_login = it },
+            label = { Text("Password") },
+            shape = RoundedCornerShape(8.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 8.dp, end = 8.dp)
+        )
+        Column(
+            horizontalAlignment = Alignment.Start,
+            modifier = modifier.padding(start = 270.dp, top = 10.dp, end = 8.dp)
+        ) { Text(text = "Forgot password?", color = Color.Blue, fontWeight = FontWeight.Bold) }
+        Button(
+            onClick = { },
+            modifier
+                .padding(start = 8.dp, end = 8.dp)
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(8.dp),
+            enabled = contraseña, //esto tengo que cambiarlo
+            colors =ButtonDefaults.buttonColors(
+                containerColor = Color(34, 111, 224, 255),
+                contentColor = Color.White,
+                disabledContainerColor = Color(89, 139, 220, 255)
+            )
+
+        ) { Text(text = "Login") }//tengo que meter aqui el validar
     }
 }
 
+@Composable
+fun SalirDeLaApp() {
+    val activity = LocalContext.current as Activity
+    activity.finish()
+}
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreviewLogin_Instagram() {
     Login_Instagram(Modifier)
 }
+
