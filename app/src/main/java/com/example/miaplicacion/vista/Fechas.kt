f5da4b3d9ae4071b3cf706538b5d99de9c88d8b9
alumnoDAM <yic00732@educastur.es>
2025-02-20 20:40:39 +0100

    modificacion de las fechas

--- app/src/main/java/com/example/miaplicacion/vista/Fechas.kt
+++ app/src/main/java/com/example/miaplicacion/vista/Fechas.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -26,6 +28,36 @@ fun Midatapicker(modifier: Modifier =Modifier){
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MidatapickerModificado(modifier: Modifier =Modifier){
+    val estadodelpicker= rememberDatePickerState()
+    val selected= estadodelpicker.selectedDateMillis?.let { converMillisFecha(it) }
+    Column(modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        DatePicker(
+            state = estadodelpicker
+        )
+        if(!selected.isNullOrEmpty()){
+            Text(text = "Fecha seleccionada $selected")
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MidatapickerModificadoTextfield(modifier: Modifier =Modifier){
+    val estadodelpicker= rememberDatePickerState()
+    val selected= estadodelpicker.selectedDateMillis?.let { converMillisFecha(it) } //?.let  comprueba que el campo  no sea null y tenga algun  tipo de dato pero  no  necesariamente  el que funciona
+    Column(modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TextField(value = selected ?: "",
+            onValueChange = {}  )
+    }
+}
+
 fun  converMillisFecha(millis: Long): String{
 val formatter = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
     return formatter.format(Date(millis))
@@ -34,5 +66,5 @@ val formatter = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreviewFechas() {
-    Midatapicker(Modifier)
+    MidatapickerModificado(Modifier)
 }
